4977e85e27f6724a672e86289c8f9a41cdf3e3e9
m2faraz <m2faraz@users.noreply.github.com>
2022-08-19 06:11:45 +0530

    added cookie session and added passport for auth

--- app.js
+++ app.js
@@ -4,26 +4,39 @@ const app = express();
 const bp = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require('mongoose-encryption');
-// const md5 = require('md5');
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const passportLocalMongoose = require('passport-local-mongoose');
+const session = require('express-session');
+const passport = require('passport');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.use(bp.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect to a database
 mongoose.connect('mongodb://localhost:27017/userDB');
 //Schema
 const userSchema = new mongoose.Schema({
-  usr: String,
-  pwd: String
+  username : String,
+  password: String
 });
 //Encryption plugin
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['pwd'] });
-//Model DB
-const User = mongoose.model("User", userSchema);
+userSchema.plugin(passportLocalMongoose);
 
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+//Model DB
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 //All get methods
 app.get("/", function(req, res){
   res.render("home");
@@ -31,7 +44,6 @@ app.get("/", function(req, res){
 
 app.get("/register", function(req, res){
   res.render("register");
-
 });
 
 app.get("/login", function(req, res){
@@ -39,68 +51,58 @@ app.get("/login", function(req, res){
 });
 
 app.get("/logout", function(req, res){
+  req.logout(function(err){
+    if(err) console.log(err);
+  });
   res.render("home");
+});
+
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+}
 });
 
 
 
 
 //all post methods
-//1. register
-app.post("/register", function(req,res){
-  console.log(req.body);
-  User.findOne({usr:req.body.username}, function(err, user){
-    if(!err){
-      if(user) res.send("<h1>User already exists</h1>");
-      else{
-        const password=req.body.password;
-        bcrypt.hash(password, saltRounds, function(err, hash) {
-          const newUser = new User({
-            usr: req.body.username,
-            pwd: hash
-          }).save(function(err){
-            if(err) console.log(err);
-            else console.log("New User added successfully");
-          });
-        });
 
-        res.render("secrets");
-      }
+app.post("/register", function(req,res){
+  User.register({username:req.body.username, active: false}, req.body.password, function(err, user) {
+    if (err){
+      console.log(err);
+      res.redirect("/register");
     }
-    else console.log(err);
-  });
+    else{
+
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets")
+      });
+    }
+});
 });
 
 app.post("/login", function(req,res){
-  User.findOne({usr:req.body.username}, function(err, user){
-    if(!err){
-      if(user){
-      const password=req.body.password;
-      bcrypt.compare(password, user.pwd, function(err, result) {
-        if(result === true){
-          res.render("secrets");
-        }
-    // if(user.pwd===password){
-    //
-    //   });
-    // }
-      else{
-      console.log("Password Incorrect. Try again with correct passowrd");
-      res.redirect("/login");
-    }
-      });
-
-    }else{
-      console.log("No user with this id found");
-      res.render("/login");
-    }
-  }
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
-});
+    req.login(user, function(err){
+      if(!err){
+        passport.authenticate("local")(req,res,function(){
+          res.redirect("/secrets")
+        });
+      }
+      else console.log(err);
+    });
+  });
 
-app.post("/submit", function(req, res){
-
-});
+// app.post("/submit", function(req, res){
+//
+// });
 
 
 
